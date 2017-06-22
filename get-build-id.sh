0643a9717be1f7a06b8a6c36e089a39f54c0af9e
Michal Mazurek <michal@resin.io>
2017-06-22 09:58:35 +0200

    Break long lines, remove trailing spaces, add trailing newlines

--- get-build-id.sh
+++ get-build-id.sh
@@ -1,6 +1,9 @@
-## This script returns the `build_id` for a specific commit on a resin.io application.
-## usage: ./get-build-id.sh <FULL_COMMIT_HASH>
+# This script returns the `build_id` for a specific commit on a resin.io application.
+# usage: ./get-build-id.sh <FULL_COMMIT_HASH>
+
 COMMIT_HASH=$1
 source resin.env
 
-curl "https://api.$BASE_URL/v2/build?\$select=id,commit_hash&\$filter=application%20eq%20$APP_ID%20and%20commit_hash%20eq%20'$COMMIT_HASH'" -H "Authorization: Bearer $authToken" | jq '.d[0].id' 
+curl "https://api.$BASE_URL/v2/build?\$select=id,commit_hash&\$filter=application%20eq%20$APP_ID%20and%20commit_hash%20eq%20'$COMMIT_HASH'" \
+    -H "Authorization: Bearer $authToken" \
+    | jq '.d[0].id'
