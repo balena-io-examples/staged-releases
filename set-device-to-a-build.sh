ab7a555af2239fa8bd08e50bf7fc8cf9230c07e1
Cameron Diver <cameron@resin.io>
2018-04-20 12:16:31 +0100

    Allow the unpinning of devices
    Change-type: patch
    Signed-off-by: Cameron Diver <cameron@resin.io>

--- set-device-to-a-build.sh
+++ set-device-to-a-build.sh
@@ -19,6 +19,10 @@ if ! [[ $DEVICE_ID =~ $IntRegex ]] && [[ $DEVICE_UUID =~ $IntRegex ]] ; then
 fi
 
 COMMIT=$2
-BUILD_ID=$(./get-build-id.sh $COMMIT)
+if [ -z $COMMIT ]; then
+	BUILD_ID="null"
+else
+	BUILD_ID=$(./get-build-id.sh $COMMIT)
+fi
 echo "setting device $DEVICE_ID to commit $COMMIT with buildID = $BUILD_ID"
 curl -X PATCH "https://api.$BASE_URL/v2/device($DEVICE_ID)" -H "Authorization: Bearer $authToken" -H "Content-Type: application/json" --data-binary '{"build":'$BUILD_ID'}'
