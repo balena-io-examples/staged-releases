d224bd93dc5231d8499d8444d0212f2e658c7e10
Pablo Carranza Velez <pablocarranza@gmail.com>
2018-09-14 14:53:39 -0700

    update-test-group: Specify group tag with $1 and commit with $2
    Change-type: major
    Signed-off-by: Pablo Carranza Velez <pablocarranza@gmail.com>

--- update-test-group.sh
+++ update-test-group.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 # This script sets a group of devices to a specific commit,
-# currently it will look for devices in an app that have an environment variable called "TEST"
-# these devices will then update to whatever commit is supplied as arg $1
+# currently it will look for devices in an app that have a tag specified as arg $1
+# these devices will then update to whatever commit is supplied as arg $2
 
 ./check-configuration.sh || exit 1
 
-COMMIT_HASH=$1
+TARGET_TAG_KEY=$1
+COMMIT_HASH=$2
 source ./resin.env
 
-TARGET_TAG_KEY='TEST'
+
 
 device_list=$(curl -X GET -H "authorization: Bearer $authToken" -H "Content-Type: application/json" "https://api.$BASE_URL/v4/device?\$expand=belongs_to__application(\$select=id),device_tag(\$select=id,tag_key)&\$filter=((belongs_to__application%20eq%20$APP_ID)%20and%20(device_tag/any(dt:((tag_key)%20eq%20(%27$TARGET_TAG_KEY%27)))))&\$select=id" | jq '.d[] | .id')
 
