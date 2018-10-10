d6275469bc1cd878ea8e04911b2175a4caf9f50a
Pablo Carranza Velez <pablocarranza@gmail.com>
2018-10-09 18:57:17 -0700

    Fixes from review, and README changes
    Signed-off-by: Pablo Carranza Velez <pablocarranza@gmail.com>

--- update-test-group.sh
+++ update-test-group.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 # This script sets a group of devices to a specific commit,
-# currently it will look for devices in an app that have a tag specified as arg $1
-# these devices will then update to whatever commit is supplied as arg $2
+# currently it will look for devices in an app that have a tag specified as arg $2
+# these devices will then update to whatever commit is supplied as arg $1
+# If a tag is not provided, it will use TEST as default.
 
 ./check-configuration.sh || exit 1
 
-TARGET_TAG_KEY=$1
-COMMIT_HASH=$2
+COMMIT_HASH=$1
+TARGET_TAG_KEY=$2
 source ./resin.env
 
 if [ -z $COMMIT_HASH ]; then
 	RELEASE_ID="null"
 else
 	RELEASE_ID=$(./get-release-id.sh $COMMIT_HASH)
+fi
+
+if [ -z "$TARGET_TAG_KEY" ]; then
+	TARGET_TAG_KEY="TEST"
 fi
 
 echo "Setting all devices with tag $TARGET_TAG_KEY to commit $COMMIT_HASH with release = $RELEASE_ID"
