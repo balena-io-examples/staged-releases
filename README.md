d7b3f989618bd6f4a7ffd8a1776d6f7a8baff3b4
Mario Camou <mcamou@tecnoguru.com>
2019-04-19 10:12:58 +0200

    Add tag values to update-test-group.sh
    Change-type: minor

--- README.md
+++ README.md
@@ -33,10 +33,10 @@ Note that you need to provide the full commit hash for this command. It should a
 
 5. For convenience, we can define a device tag with `TEST` (or any other tag) as its key that is used to designate the devices that are part of the testing group. So any device in the fleet that has the `TEST` tag can then be set with a testing build. The `update-test-group.sh` script will set all the devices with this tag to a specific commit/build:
 ```
-./update-test-group.sh <FULL COMMIT HASH HERE> <TAG NAME>
+./update-test-group.sh <FULL COMMIT HASH HERE> [<TAG NAME> [<TAG_VALUE>]]
 ```
 
-Where `<TAG NAME>` defaults to `TEST` if not provided.
+Where `<TAG NAME>` defaults to `TEST` if not provided. If `<TAG_VALUE>` is omitted, it will update all devices that have that tag.
 
 Note: this must be the full commit hash. It may take a few seconds for the test devices to start updating to the correct version of the code.
 
--- update-test-group.sh
+++ update-test-group.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 # This script sets a group of devices to a specific commit,
-# currently it will look for devices in an app that have a tag specified as arg $2
+# currently it will look for devices in an app that have a tag specified as arg $2, optionally with a specific value specified as arg $3
 # these devices will then update to whatever commit is supplied as arg $1
-# If a tag is not provided, it will use TEST as default.
+# If a tag is not provided, it will use the TEST tag with any value as default.
 
 ./check-configuration.sh || exit 1
 
 COMMIT_HASH=$1
 TARGET_TAG_KEY=$2
+TARGET_TAG_VALUE=$3
+
 if [ -f "./resin.env" ]; then
     source ./resin.env
 fi
@@ -23,8 +25,16 @@ if [ -z "$TARGET_TAG_KEY" ]; then
 	TARGET_TAG_KEY="TEST"
 fi
 
-echo "Setting all devices with tag $TARGET_TAG_KEY to commit $COMMIT_HASH with release = $RELEASE_ID"
+if [ ! -z "$TARGET_TAG_VALUE" ] ; then
+  VALUE_QUERY="and%20(device_tag/any(dt:((tag_value)%20eq%20(%27$TARGET_TAG_VALUE%27))))"
+  VALUE_MSG="and value $TARGET_TAG_VALUE"
+else
+  VALUE_MSG="with any value"
+fi
+
+echo "Setting all devices with tag $TARGET_TAG_KEY $VALUE_MSG to commit $COMMIT_HASH with release = $RELEASE_ID"
+
 curl -X PATCH -H "authorization: Bearer $authToken"\
   -H "Content-Type: application/json" \
 	--data-binary '{"should_be_running__release":'$RELEASE_ID'}' \
-	"https://api.$BASE_URL/v5/device?\$expand=belongs_to__application(\$select=id),device_tag(\$select=id,tag_key)&\$filter=((belongs_to__application%20eq%20$APP_ID)%20and%20(device_tag/any(dt:((tag_key)%20eq%20(%27$TARGET_TAG_KEY%27)))))"
+	"https://api.$BASE_URL/v5/device?\$expand=belongs_to__application(\$select=id),device_tag(\$select=id,tag_key)&\$filter=((belongs_to__application%20eq%20$APP_ID)%20and%20(device_tag/any(dt:((tag_key)%20eq%20(%27$TARGET_TAG_KEY%27))))$VALUE_QUERY)"
